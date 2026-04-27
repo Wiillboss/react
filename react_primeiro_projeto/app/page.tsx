20af5f3dae89cbd226b947bf895af4f2b5d83d34
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2026-04-26 22:16:03 -0300

    criando um novo projeto
    Foi excluído o repositório anterior e criado um novo, visto que o anterior foi criado um alguns erros, nesse não foi criado o projeto com VITE, somente com Next e está funcioando

---
+++ react_primeiro_projeto/app/page.tsx
@@ -0,0 +1,21 @@
+function Page(){
+  return(
+    <div>
+      <h1 className="font-bold text-2xl">Ola mundo!</h1>
+      <h3>Seja bem vindo</h3>
+    </div>
+  );
+}
+
+//criando uma arrow function
+const Page_dois = () => {
+  return(
+    <div>
+      <h1 className="font-bold text-2xl">Ola mundo, de novo!</h1>
+      <h3>Seja bem vindo, novamente.</h3>
+    </div>
+  );
+}
+
+//export default Page;
+export default Page_dois;
