a74077faa53a47caa313b536123dc4984c9103b6
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2026-05-11 22:17:15 -0300

    adicionando margin

--- react_primeiro_projeto/app/estados_eventos/page.tsx
+++ react_primeiro_projeto/app/estados_eventos/page.tsx
@@ -12,13 +12,13 @@ const Page_estados_eventos_click = () => {
     return (
         <div className="w-screen h-screen flex justify-center items-center">     
             <button
-                className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700"
+                className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 m-3"
                 onClick={handleClick}>
                 Clique aqui - Botão 1
             </button>
             
             <button
-                className="px-4 py-2 bg-green-600 text-white rounded-md hover:bg-green-700"
+                className="px-4 py-2 bg-green-600 text-white rounded-md hover:bg-green-700 m-3"
                 onClick={function click() {
                     alert("Botão clicado 2!");
                 }}>
@@ -26,13 +26,13 @@ const Page_estados_eventos_click = () => {
             </button>
 
             <button
-                className="px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-700"
+                className="px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-700 m-3"
                 onClick={() => alert("Botão clicado 3!")}>
                 Clique aqui - Botão 3
             </button>
 
             <button
-                className="px-4 py-2 bg-purple-600 text-white rounded-md hover:bg-purple-700"
+                className="px-4 py-2 bg-purple-600 text-white rounded-md hover:bg-purple-700 m-3"
                 onClick={handleClick2}>
                 Clique aqui - Botão 4
             </button>
