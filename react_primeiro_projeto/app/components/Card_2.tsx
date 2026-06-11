177757f0874fdfa5e97dc488b9622d9fb2850745
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2026-06-11 20:58:51 -0300

    atualização de comentários

--- react_primeiro_projeto/app/components/Card_2.tsx
+++ react_primeiro_projeto/app/components/Card_2.tsx
@@ -9,8 +9,54 @@ export const Card2 = ({ phrase, author }: Props) => {
     return (
         <div className="w-96 h-40 bg-red-600 text-white p-3 text-3xl text-center italic">
             <h3 className="text-3xl font-bold italic">{phrase}</h3>
-            <p className="text-right text-sm">— {author?? "Autor Desconhecido"}</p>
+            <p className="text-right text-sm">— {author ?? "Autor Desconhecido"}</p>
         </div>
     );
 
 }
+
+// O operador de coalescência nula (??) é usado para fornecer um valor padrão caso o valor de author seja null ou undefined. Se author for fornecido, ele será exibido; caso contrário, "Autor Desconecido" será mostrado.
+
+//Outras formas de fazer o IF nesse caso:
+//Abaixo estão algumas formas alternativas de implementar a lógica para exibir o autor ou "Autor Desconecido" sem usar o operador de coalescência nula:
+// if (author) {
+//     return (
+//         <div className="w-96 h-40 bg-red-600 text-white p-3 text-3xl text-center italic">
+//             <h3 className="text-3xl font-bold italic">{phrase}</h3>
+//             <p className="text-right text-sm">— {author}</p>
+//         </div>
+//     );
+// } else {
+//     return (
+//         <div className="w-96 h-40 bg-red-600 text-white p-3 text-3xl text-center italic">
+//             <h3 className="text-3xl font-bold italic">{phrase}</h3>
+//             <p className="text-right text-sm">— Autor Desconecido</p>
+//         </div>
+//     );
+// }
+
+
+//Abaixo está uma forma mais concisa usando um operador ternário para decidir o que exibir com base na presença do autor:
+// if (author) {
+//     return (
+//         <div className="w-96 h-40 bg-red-600 text-white p-3 text-3xl text-center italic">
+//             <h3 className="text-3xl font-bold italic">{phrase}</h3>
+//             <p className="text-right text-sm">— {author}</p>
+//         </div>
+//     );
+// }
+//   return (
+//         <div className="w-96 h-40 bg-red-600 text-white p-3 text-3xl text-center italic">
+//             <h3 className="text-3xl font-bold italic">{phrase}</h3>
+//             <p className="text-right text-sm">— Autor Desconecido</p>
+//         </div>
+//     );
+// }
+
+//abaixo está sendo feito com if ternário, que é uma forma mais compacta de escrever um if-else. Ele verifica se author existe e, se sim, exibe o nome do autor; caso contrário, exibe "Autor Desconecido".
+// return (
+//     <div className="w-96 h-40 bg-red-600 text-white p-3 text-3xl text-center italic">    
+//         <h3 className="text-3xl font-bold italic">{phrase}</h3>
+//         <p className="text-right text-sm">— {author ? author : "Autor Desconecido"}</p>
+//     </div>
+// );
