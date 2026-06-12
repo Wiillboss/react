ab8efae8d51726a9aff58504a49515fc87707012
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2026-06-11 21:12:35 -0300

    atualizando

--- react_primeiro_projeto/app/components/Card_3_and.tsx
+++ react_primeiro_projeto/app/components/Card_3_and.tsx
@@ -4,6 +4,7 @@ type Props = {
 };
 
 // Componente: Card_3 (exportado como padrão)
+// O componente Card_3 é uma função que recebe as props "phrase" e "autor". Ele retorna um JSX.Element que representa um card estilizado com Tailwind CSS. O card exibe a frase e, se o autor for fornecido, exibe o nome do autor; caso contrário, exibe "Autor desconhecido". A estrutura do card inclui uma borda cinza, texto centralizado e estilo itálico.
 export const Card_3 = ({ phrase, autor }: Props) => {
     return (
         <div className="w-80 border-2 border-gray-600 text-center italic p-4">
