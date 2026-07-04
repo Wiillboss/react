4d4d24750cc4e112da572b13c962167229093c1b
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2026-07-03 22:05:59 -0300

    comentários

--- react_primeiro_projeto/app/components/Card_3_and.tsx
+++ react_primeiro_projeto/app/components/Card_3_and.tsx
@@ -5,6 +5,10 @@ type Props = {
 
 // Componente: Card_3 (exportado como padrão)
 // O componente Card_3 é uma função que recebe as props "phrase" e "autor". Ele retorna um JSX.Element que representa um card estilizado com Tailwind CSS. O card exibe a frase e, se o autor for fornecido, exibe o nome do autor; caso contrário, exibe "Autor desconhecido". A estrutura do card inclui uma borda cinza, texto centralizado e estilo itálico.
+
+// Exportação do componente Card_3 usando named export, permitindo que seja importado em outros arquivos usando import { Card_3 } from "./components/Card_3_and"
+
+//explicando que ter mais de um return em um componente é possível, mas não é uma prática recomendada, pois pode gerar confusão e dificultar a manutenção do código. É preferível ter um único return por componente para manter a clareza e a simplicidade do código.
 export const Card_3 = ({ phrase, autor }: Props) => {
     return (
         <div className="w-80 border-2 border-gray-600 text-center italic p-4">
