900c6a4b99f47938791d1f1cc08bd554eb85e3a5
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2026-07-25 11:34:46 -0300

    comentários

--- react_primeiro_projeto/app/types/Pessoa.ts
+++ react_primeiro_projeto/app/types/Pessoa.ts
@@ -1,4 +1,7 @@
-
+//explicando detalhadamente o arquivo Pessoa.ts e por qual motivo ele foi criado:
+// Este arquivo contém a definição do tipo Pessoa, que é usado para representar uma pessoa com informações mais detalhadas.
+// O tipo Pessoa possui quatro propriedades: id (um número que representa o identificador único da pessoa), nome (uma string que representa o nome completo da pessoa), profissao (uma string que representa a profissão da pessoa) e sexo (uma string que representa o sexo da pessoa).
+// Ele foi criado para fornecer uma estrutura clara e consistente para representar informações de pessoas, permitindo que outros componentes ou arquivos do projeto possam importar e utilizar esse tipo de forma segura e eficiente.
 export type Pessoa = {
   id: number;
   nome: string;
