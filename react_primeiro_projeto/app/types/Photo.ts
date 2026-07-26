e808321fcc6838b4eb7570e0a4665aa1bf3fd136
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2026-07-26 18:52:49 -0300

    comentários

--- react_primeiro_projeto/app/types/Photo.ts
+++ react_primeiro_projeto/app/types/Photo.ts
@@ -1,3 +1,8 @@
+//explicando detalhadamente o arquivo Photo.ts e por qual motivo ele foi criado:
+// Este arquivo contém a definição do tipo Foto, que é usado para representar uma foto com informações básicas.
+// O tipo Foto possui duas propriedades: id (um número que representa o identificador único da foto) e url (uma string que representa o endereço da foto).
+// Ele foi criado para fornecer uma estrutura clara e consistente para representar informações de fotos, permitindo que outros componentes ou arquivos do projeto possam importar e utilizar esse tipo de forma segura e eficiente.
+
 export type Foto = {
     id: number;
     url: string;
