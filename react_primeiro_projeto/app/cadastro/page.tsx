9133966c0dc39dd8984a7ec43dfb3b00a0fb49df
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2026-08-05 21:24:45 -0300

    atualização

--- react_primeiro_projeto/app/cadastro/page.tsx
+++ react_primeiro_projeto/app/cadastro/page.tsx
@@ -2,19 +2,21 @@
 
 const Page = () => {
   return (
-    <div>
-        <h1 className="">Cadastro</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-800 text-white">
+        <h1 className="text-3xl font-bold mb-4">Cadastro</h1>
         <form>
             <label htmlFor="nome">Nome:</label>
-            <input type="text" id="nome" name="nome" required />
+            <input className="bg-white text-gray-800 rounded px-3 py-2" type="text" id="nome" name="nome" required />
             <br />
             <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" required />
+            <input className="bg-white text-gray-800 rounded px-3 py-2" type="email" id="email" name="email" required />
             <br />
             <label htmlFor="senha">Senha:</label>
-            <input type="password" id="senha" name="senha" required />
+            <input className="bg-white text-gray-800 rounded px-3 py-2" type="password" id="senha" name="senha" required />
             <br />
-            <button type="submit">Cadastrar</button>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">
+                Cadastrar
+            </button>
         </form>
     </div>
   );
