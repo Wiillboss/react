a125a162fb4d613efa20997c01afac214fed7670
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2026-08-15 20:13:11 -0300

    mudanças

--- react_primeiro_projeto/app/cadastro/page.tsx
+++ react_primeiro_projeto/app/cadastro/page.tsx
@@ -88,16 +88,11 @@ const Page = () => {
                     type="submit">
                     Entrar
                 </button>
-
-                <br/>
-
-                <label htmlFor="nome" className="font-bold">Não possui conta?</label>
                 
-                <br/>
                 <button
                     className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                     type="submit">
-                    Cadastrar
+                    Cadastrar Usuário 
                 </button>
             </form>
         </div>
