6c5e82f710a36af4ab293c56a7e422faa19f2ae3
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2026-09-08 19:01:08 -0300

    mudanças que não deram certo

--- react_primeiro_projeto/app/components/Tela_login_melhorada.tsx
+++ react_primeiro_projeto/app/components/Tela_login_melhorada.tsx
@@ -7,6 +7,10 @@
 
 //explicando que com a responsividade do tailwindcss, podemos usar classes como "sm:", "md:", "lg:", "xl:" e "2xl:" para aplicar estilos diferentes em diferentes tamanhos de tela. Por exemplo, "sm:w-full" aplicaria a largura completa em telas pequenas, enquanto "md:w-1/2" aplicaria metade da largura em telas médias.
 
+//explicando como colocar outro componente como endereço de destino após clicar em um botão, podemos usar a tag <a> com o atributo href, ou podemos usar o componente Link do Next.js para navegação interna. Por exemplo, <Link href="/cadastro">Cadastre-se</Link>.
+
+import Link from "next/link";
+
 export const Tela_login_melhorada = () => {
     return (
       <div className= "w-screen h-screen p-5 bg-gray-900">
@@ -29,7 +33,7 @@ export const Tela_login_melhorada = () => {
         </div>
         
         <div className="text-center mt-4 text-gray-400">
-            Não é membro? <a href="/cadastro" className="text-blue-500 hover:text-blue-700">Cadastre-se</a>
+            Não é membro? <Link href="/FormPessoaCadastro" className="text-blue-500 hover:text-blue-700">Cadastre-se</Link>
         </div>
       </div>  
     );
