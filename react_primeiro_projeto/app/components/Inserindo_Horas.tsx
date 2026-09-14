030254193d342cb115b3b9731bd82cdb9441b4ab
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2026-09-13 21:57:41 -0300

    melhorando

--- react_primeiro_projeto/app/components/Inserindo_Horas.tsx
+++ react_primeiro_projeto/app/components/Inserindo_Horas.tsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 
 export const Inserindo_Horas = () => {
+    const [mostrarHorasFora, setMostrarHorasFora] = useState(false);
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-800 text-white">
             <h1 className="text-3xl font-bold mb-4">Inserção de Horas</h1>
-            <form
-                className="bg-gray-700 p-6 rounded shadow-md w-lg"
-            >
+
+            <form className="bg-gray-700 p-6 rounded shadow-md w-lg">
+
                 <div className="mb-4">
                     <label className="block text-lg mb-2" htmlFor="employeeId">
                         ID do Funcionário
@@ -17,6 +20,7 @@ export const Inserindo_Horas = () => {
                         placeholder="Digite o ID do funcionário"
                     />
                 </div>
+
                 <div className="mb-4">
                     <label className="block text-lg mb-2" htmlFor="data">
                         Data
@@ -25,9 +29,9 @@ export const Inserindo_Horas = () => {
                         className="w-full p-2 rounded-md text-lg bg-gray-600 border border-gray-500 outline-blue-500"
                         type="date"
                         id="data"
-                        placeholder="Digite a data"
                     />
                 </div>
+
                 <div className="mb-4">
                     <label className="block text-lg mb-2" htmlFor="entrada">
                         Entrada
@@ -36,9 +40,9 @@ export const Inserindo_Horas = () => {
                         className="w-full p-2 rounded-md text-lg bg-gray-600 border border-gray-500 outline-blue-500"
                         type="time"
                         id="entrada"
-                        placeholder="Digite a hora de entrada"
                     />
                 </div>
+
                 <div className="mb-4">
                     <label className="block text-lg mb-2" htmlFor="entrada_almoco">
                         Entrada Almoço
@@ -47,9 +51,9 @@ export const Inserindo_Horas = () => {
                         className="w-full p-2 rounded-md text-lg bg-gray-600 border border-gray-500 outline-blue-500"
                         type="time"
                         id="entrada_almoco"
-                        placeholder="Digite a hora de entrada"
                     />
                 </div>
+
                 <div className="mb-4">
                     <label className="block text-lg mb-2" htmlFor="saida_almoco">
                         Saída Almoço
@@ -58,9 +62,9 @@ export const Inserindo_Horas = () => {
                         className="w-full p-2 rounded-md text-lg bg-gray-600 border border-gray-500 outline-blue-500"
                         type="time"
                         id="saida_almoco"
-                        placeholder="Digite a hora de saída"
                     />
                 </div>
+
                 <div className="mb-4">
                     <label className="block text-lg mb-2" htmlFor="saida">
                         Saída
@@ -69,25 +73,44 @@ export const Inserindo_Horas = () => {
                         className="w-full p-2 rounded-md text-lg bg-gray-600 border border-gray-500 outline-blue-500"
                         type="time"
                         id="saida"
-                        placeholder="Digite a hora de saída"
                     />
                 </div>
-                <div className=" flex justify-around mb-4">
+
+                {/* Input que aparece ao clicar no botão */}
+                {mostrarHorasFora && (
+                    <div className="mb-4">
+                        <label className="block text-lg mb-2" htmlFor="horasFora">
+                            Horas Fora do Horário
+                        </label>
+
+                        <input
+                            className="w-full p-2 rounded-md text-lg bg-gray-600 border border-gray-500 outline-blue-500"
+                            type="time"
+                            id="horasFora"
+                            placeholder="Digite as horas"
+                        />
+                    </div>
+                )}
+
+                <div className="flex gap-4 justify-around mb-4">
+
                     <button
                         className="w-full p-2 bg-blue-500 text-white rounded-md hover:bg-blue-700 font-bold cursor-pointer"
                         type="submit"
                     >
                         Inserir Horas
                     </button>
+
                     <button
-                        onClick={() => alert("Hora fora do horário inserida!")}
+                        onClick={() => setMostrarHorasFora(!mostrarHorasFora)}
                         className="w-full p-2 bg-blue-500 text-white rounded-md hover:bg-blue-700 font-bold cursor-pointer"
-                        type="submit"
+                        type="button"
                     >
                         Horas Fora do Horário
                     </button>
+
                 </div>
             </form>
         </div>
     );
-}
+};
