0c5743f44c0697b0c82a41f7884cc124dbf12001
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2026-09-14 20:46:54 -0300

    comentários

--- react_primeiro_projeto/app/components/Inserindo_Horas.tsx
+++ react_primeiro_projeto/app/components/Inserindo_Horas.tsx
@@ -1,5 +1,13 @@
 import { useState } from "react";
 
+
+//explicando de forma didatica o componente acima:
+//1. O componente Inserindo_Horas é um componente funcional do React que permite aos usuários inserir informações de horas trabalhadas.
+//2. Ele utiliza o hook useState para gerenciar o estado da exibição do campo "Horas Fora do Horário".
+
+//3. O componente retorna um formulário estilizado com Tailwind CSS, contendo campos para inserir o ID do funcionário, data, horários de entrada e saída, e um campo opcional para "Horas Fora do Horário".
+
+//4. O botão "Horas Fora do Horário" alterna a exibição do campo correspondente, permitindo que o usuário insira informações adicionais se necessário.
 export const Inserindo_Horas = () => {
     const [mostrarHorasFora, setMostrarHorasFora] = useState(false);
 
@@ -92,6 +100,7 @@ export const Inserindo_Horas = () => {
                     </div>
                 )}
 
+                {/*Explicando a div abaixo:  */}
                 <div className="flex gap-4 justify-around mb-4">
 
                     <button
